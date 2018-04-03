9d33236bc9e72a2b6b26719c9ab5190d97216f91
Leon Kampen <leonvkampen@gmail.com>
2018-04-03 09:41:17 +0200

    Home +logo

---
+++ dating/index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Borrow - is the loan company website template.">
+    <meta name="keywords" content="loan, bad credit, EMI Calculator, credit cars, home loan, car loan, education loan">
+    <title>Borrow - A Loan Company Website Templates</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/fontello.css" rel="stylesheet">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Merriweather:300,300i,400,400i,700,700i"
+          rel="stylesheet">
+    <!-- owl Carousel Css -->
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<?php include 'assets/header.html' ?>
+
+<div class="section-space80">
+    <div class="container">
+        <div class="row">
+            <div class="offset-xl-2 col-xl-8 offset-lg-2 col-lg-8 col-md-12 col-sm-12 col-12 ">
+                <div class="mb60 text-center section-title">
+                    <div style="margin-top:5%"></div>
+                    <!-- section title start-->
+                    <h1>Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae vestibulum urna. Sed dictum
+                        lobortis lacus, id feugiat elit aliquet at. Proin eget ultrices tellus, sit amet accumsan elit.
+                        Nulla blandit non sapien varius ullamcorper. Pellentesque ultrices efficitur pulvinar.
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In
+                        pretium arcu et tortor dictum, ac gravida velit fermentum. Donec ultrices enim eu ligula
+                        lobortis, vel mattis nunc fermentum.</p>
+                </div>
+                <!-- /.section title start-->
+            </div>
+        </div>
+
+        <div class="" id="service">
+            <div class="row">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-12">
+                    <div class="service-block-v3 service-block-1">
+                        <div class="service-block-backround" style="background-image: url(images/hetero.jpg);"></div>
+                        <div class="category">
+                            <h2 class="service-title"><a href="#" class="title"><strong>Hetero</strong></a></h2>
+                        </div>
+                        <a style="display:block" href="about.php">
+                            <div class="overlay-home overlay-home-1">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top1
+                                </h3>
+                            </div>
+                            <div class="overlay-home overlay-home-2">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top
+                                </h3>
+                            </div>
+                            <div class="overlay-home overlay-home-3">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top
+                                </h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-12">
+                    <div class="service-block-v3 service-block-2">
+                        <div class="service-block-backround"
+                             style="background-image: url(images/vreemdgaan.jpg);"></div>
+                        <div class="category">
+                            <h2 class="service-title"><a href="#" class="title"><strong></bolt>Vreemdgaan</strong></a>
+                            </h2>
+                        </div>
+                        <a style="display:block" href="about.php">
+                            <div class="overlay-home overlay-home-1">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top</h3>
+                            </div>
+                            <div class="overlay-home overlay-home-2">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top</h3>
+                            </div>
+                            <div class="overlay-home overlay-home-3">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top</h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-12">
+                    <div class="service-block-v3">
+                        <div class="service-block-backround" style="background-image: url(images/lesbisch.jpg);"></div>
+                        <div class="category">
+                            <h2 class="service-title"><a href="#" class="title"><strong>Lesbisch</strong></a></h2>
+                        </div>
+                        <a style="display:block" href="about.php">
+                            <div class="overlay-home overlay-home-1">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top</h3>
+                            </div>
+                            <div class="overlay-home overlay-home-2">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top</h3>
+                            </div>
+                            <div class="overlay-home overlay-home-3">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top</h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-12">
+                    <div class="service-block-v3">
+                        <div class="service-block-backround" style="background-image: url(images/homo.jpg);"></div>
+                        <div class="category">
+                            <h2 class="service-title"><a href="#" class="title"><strong>Gay</strong></a></h2>
+                        </div>
+                        <a style="display:block" href="about.php">
+                            <div class="overlay-home overlay-home-1">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top></h3>
+                            </div>
+                            <div class="overlay-home overlay-home-2">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top</h3>
+                            </div>
+                            <div class="overlay-home overlay-home-3">
+                                <h3 class="title"
+                                    style="text-align: center; vertical-align: middle; line-height: 90px;">top</h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="mb60 text-center section-title" style="margin-top:30%">
+            <div style="margin-top:5%"></div>
+            <!-- section title start-->
+            <h1>Lorem ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae vestibulum urna. Sed dictum lobortis
+                lacus, id feugiat elit aliquet at. Proin eget ultrices tellus, sit amet accumsan elit. Nulla blandit non
+                sapien varius ullamcorper. Pellentesque ultrices efficitur pulvinar. Pellentesque habitant morbi
+                tristique senectus et netus et malesuada fames ac turpis egestas. In pretium arcu et tortor dictum, ac
+                gravida velit fermentum. Donec ultrices enim eu ligula lobortis, vel mattis nunc fermentum.</p>
+        </div>
+        <!-- /.section title start-->
+    </div>
+</div>
+
+
+<?php include 'assets/footer.html' ?>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/menumaker.js"></script>
+
+<!-- sticky header -->
+<script type="text/javascript" src="js/jquery.sticky.js"></script>
+<script type="text/javascript" src="js/sticky-header.js"></script>
+<!-- Back to top script -->
+<script src="js/back-to-top.js" type="text/javascript"></script>
+</body>
+
+</html>
